dc23b8662cd34d49947872363569ccfe0289af21
sekardewiy <sekar2100016107@webmail.uad.ac.id>
2023-07-18 22:04:19 +0700

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "bulma/css/bulma.css";
+import { createRoot } from "react-dom/client"; // Mengimpor createRoot dari react-dom/client
+
+import App from "./App";
+
+createRoot(document.getElementById("root")).render( // Menggunakan createRoot dengan render
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
