4a3dd0354be712df56be5aa79cd57bf691f0eba4
xinzzu <azisnurrahman54@gmail.com>
2023-07-22 21:15:29 +0700

    be

--- server/db.js
+++ server/db.js
@@ -7,7 +7,7 @@ module.exports = () => {
 	};
 	try {
 		mongoose.connect(process.env.DB, connectionParams);
-		console.log("Connected to database successfully");
+		console.log("Connected to database berhasil");
 	} catch (error) {
 		console.log(error);
 		console.log("Could not connect database!");
